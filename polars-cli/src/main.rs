0f57de55a5ed7f016c265b2e210f6c3ca3057c34
Noah Gift <noah.gift@gmail.com>
2023-08-17 20:04:45 +0000

    adding polars cli

---
+++ polars-cli/src/main.rs
@@ -0,0 +1,21 @@
+/*
+CLI interface for Polars.  Calls lib.rs
+*/
+use clap::Parser;
+use polars_cli::calculate;
+
+#[derive(Parser)]
+#[command(
+    author,
+    version,
+    about,
+    long_about = "A CLI tool that wraps Polars code."
+)]
+struct Cli {
+}
+
+fn main() {
+    let _args = Cli::parse();
+    calculate().unwrap();
+}
+
